c3a177a1ac0a98fdd0260fb171eccc97f4b54995
Reuben Settergren <ruberad@gmail.com>
2017-09-04 13:53:36 -0700

    made trees more relative made center of houses on top of center of leaves made bridges relative to top of leaves

--- theMasterOne.py
+++ theMasterOne.py
@@ -14,14 +14,15 @@ x = round(pos.x)
 z = round(pos.z)
 y = round(pos.y)
 
-x = 1000000
+x = 100100
 y = 4
-z = 1000000
+z = 100000
 
 def treeHouse(mc, x, y, z):
-    treeBuilder.treeBuilder(mc, x, y, z, 2)
-    lothlorienHouse.buildHouse(mc, x - 2, y + 52, z - 3, 5)
+    treeTop = treeBuilder.treeBuilder(mc, x, y, z)
+    lothlorienHouse.buildHouse(mc, x, treeTop, z, 5)
+    return treeTop
 
-treeHouse(mc, x + 10, y, z)
-treeHouse(mc, x + 30, y, z + 60)
-Bridge.bridge(mc, x + 10, y + 52, z, x + 30, y + 52, z + 60, 17, railings=False)
+treeTop1 = treeHouse(mc, x + 10, y, z)
+treeTop2 = treeHouse(mc, x + 30, y, z + 60)
+Bridge.bridge(mc, x + 10, treeTop1, z, x + 30, treeTop2, z + 60, 17, railings=False)
