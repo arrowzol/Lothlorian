92d344cfecb8a06b574b8ae0a529aa53011fb1a5
CalebPak10 <calebpak10@gmail.com>
2017-09-04 16:32:31 -0700

    It now has an optional parabola shape. The bridge can now be set so that it has a parameter that lets you set how much the bridge can dip down to.

--- Bridge.py
+++ Bridge.py
@@ -4,7 +4,8 @@ GOLD = 41
 LEAVES = 18
 TNT = 49
 
-def line(mc, x1, y1, z1, x2, y2, z2, btype=GOLD, stype=None):
+
+def line(mc, x1, y1, z1, x2, y2, z2, dip=0, btype=GOLD, stype=None):
     dx = x2 - x1
     dy = y2 - y1
     dz = z2 - z1
@@ -16,13 +17,16 @@ def line(mc, x1, y1, z1, x2, y2, z2, btype=GOLD, stype=None):
     dz /= steps
 
     for i in range(steps + 1):
+        f = i / steps
+        g = 1 - i/steps
+        y_dip = 4*dip*f*g
         if stype:
-            mc.setBlock(x1 + dx*i, y1 + dy*i, z1 + dz*i, btype, stype)
+            mc.setBlock(int(round(x1 + dx*i)), int(round(y1 + dy*i - y_dip)), int(round(z1 + dz*i)), btype, stype)
         else:
-            mc.setBlock(x1 + dx*i, y1 + dy*i, z1 + dz*i, btype)
+            mc.setBlock(int(round(x1 + dx*i)), int(round(y1 + dy*i - y_dip)), int(round(z1 + dz*i)), btype)
 
 
-def bridge(mc, x1, y1, z1, x2, y2, z2, width=4, railings=True, btype=GOLD, stype=None):
+def bridge(mc, x1, y1, z1, x2, y2, z2, width=4, dip=0, railings=True, btype=GOLD, stype=None):
     # calculate distance between two points, in 2D
     dx = x2 - x1
     dz = z2 - z1
@@ -38,14 +42,14 @@ def bridge(mc, x1, y1, z1, x2, y2, z2, width=4, railings=True, btype=GOLD, stype
 
     # main path
     for i in range(-width*2, width*2+1):
-        line(mc, x1 + ux*i, y1, z1 + uz*i, x2 + ux*i, y2, z2 + uz*i)
+        line(mc, x1 + ux*i, y1, z1 + uz*i, x2 + ux*i, y2, z2 + uz*i, dip, btype, stype)
 
     # railings
     if railings:
         i = -width*2
-        line(mc, x1 + ux * i, y1 + 2, z1 + uz * i, x2 + ux * i, y2 + 2, z2 + uz * i, btype, stype)
+        line(mc, x1 + ux * i, y1 + 2, z1 + uz * i, x2 + ux * i, y2 + 2, z2 + uz * i, dip, btype, stype)
         i = width*2
-        line(mc, x1 + ux * i, y1 + 2, z1 + uz * i, x2 + ux * i, y2 + 2, z2 + uz * i, btype, stype)
+        line(mc, x1 + ux * i, y1 + 2, z1 + uz * i, x2 + ux * i, y2 + 2, z2 + uz * i, dip, btype, stype)
 
 
 if __name__ == "__main__":
@@ -54,4 +58,4 @@ if __name__ == "__main__":
     mc = remoteMCServer.create("")
 
     pos = mc.player.getPos()
-    bridge(mc, pos.x-10, pos.y+2, pos.z-10, pos.x+10, pos.y+5, pos.z+15, 4)
+    bridge(mc, pos.x-10, pos.y+2, pos.z-10, pos.x+10, pos.y+5, pos.z+15, 4, dip=5)
