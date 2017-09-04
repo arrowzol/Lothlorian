722fa5dfe82b9bcc7de37c3c81f72b15ba5d18ca
bobgo1111 <curtis.b@earthlink.net>
2017-09-04 12:02:10 -0700

    makes two houses and connects them with golden bridge.

--- theMasterOne.py
+++ theMasterOne.py
@@ -4,7 +4,7 @@
 
 import remoteMCServer
 
-import lothlorienHouse, treeBuilder
+import lothlorienHouse, treeBuilder, Bridge
 
 mc = remoteMCServer.create("")
 
@@ -14,7 +14,14 @@ x = round(pos.x)
 z = round(pos.z)
 y = round(pos.y)
 
+x = 1000000
+y = 4
+z = 1000000
 
 def treeHouse(mc, x, y, z):
     treeBuilder.treeBuilder(mc, x, y, z, 2)
-    lothlorienHouse.buildHouse(mc, x - 2, y + 52, z  - 3, 5)
+    lothlorienHouse.buildHouse(mc, x - 2, y + 52, z - 3, 5)
+
+treeHouse(mc, x + 10, y, z)
+treeHouse(mc, x + 30, y, z + 60)
+Bridge.bridge(mc, x + 10, y + 52, z, x + 30, y + 52, z + 60, 17, railings=False)
