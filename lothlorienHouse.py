bd69126f13f41df5072c636309bea00bb95ee10b
Reuben Settergren <RubeRad@gmail.com>
2017-09-04 13:05:04 -0700

    THE PORCH IS FINALLY CIRCULAR!!!!!!

--- lothlorienHouse.py
+++ lothlorienHouse.py
@@ -11,14 +11,8 @@ def buildHouse(mc,ctrx,y,ctrz,width):
     x = ctrx - int(width / 2)
     z = ctrz - int(length / 2)
 
-
     air = 0
     doorx = x + int(width * 0.5)
-
-
-
-
-
 
     #make a cuboid
     mc.setBlocks(x, y, z, x + width-1, y + height-1, z + length-1, 5,2)
@@ -26,6 +20,7 @@ def buildHouse(mc,ctrx,y,ctrz,width):
     mc.setBlocks(x+1,y+1,z+1,x+width-2,y+height-2,z+length-2,air)
     #add carpets
     mc.setBlocks(x + 1, y + 1, z + 1, x + width - 2, y+1, z + length - 2, 171,13)
+
     #make layer 1 of roof with overhang
     roofx = x - 1
     roofy = y + height
@@ -35,17 +30,13 @@ def buildHouse(mc,ctrx,y,ctrz,width):
         roofx += 1
         roofy += 1
         roofw -= 2
-    #2 part door, torcch, and windows
+    #2 part door, torch, and windows
     mc.setBlock(doorx, y + 1, z, 193)
     mc.setBlock(doorx, y + 2, z, 193)
     mc.setBlock(doorx, y + 3, z + 1, 5,2)
     mc.setBlock(doorx, y + 4, z + 1, 50)
     mc.setBlocks(x, y + 2, z + 2, x, y + 3, z + int(5/7 * length), 102)
     mc.setBlocks(x + width - 1, y + 2, z + 2, x + width - 1, y + 3, z + int(5 / 7 * length), 102)
-
-
-
-
 
     #circular porch
     #radius of circle is different from center to corner
@@ -57,7 +48,7 @@ def buildHouse(mc,ctrx,y,ctrz,width):
         for dz in range(-r,r+1):
             if dx*dx + dz*dz <= r*r:#pythagoras
                 mc.setBlock(ctrx + dx,y,ctrz + dz, 5,2)
-    #mc.setBlocks(x + width - 1)
+    
 
 
 
